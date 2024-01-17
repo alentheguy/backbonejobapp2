d8e2f9b0a9b7c6eca42e60603e79702d47c7ea4f
alentheguy <alvinw0231@gmail.com>
2024-01-16 21:52:32 -0600

    Created basic template for pages and such

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import InputForm 
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def datascientist(request):
+    return render(request, 'datascientist.html')
+
+def home_view(request): 
+    context ={} 
+    context['form']= InputForm() 
+    return render(request, "home.html", context) 
