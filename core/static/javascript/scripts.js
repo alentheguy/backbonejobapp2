6fbacbdb70cf57692580185528c0ff5ea79dfd4f
Amar <94648238+Destroyer3po@users.noreply.github.com>
2024-01-27 02:00:11 -0600

    fixed capston
    fixed capston

--- core/static/javascript/scripts.js
+++ core/static/javascript/scripts.js
@@ -2,26 +2,27 @@ function showRoleDropdown() {
     var pdfSubmissionDropdown = document.getElementById("pdfSubmissionDropdown");
     var selectedOption = document.getElementById("pdfSubmission").value;
     var dataScientistFields = document.getElementById("dataScientistFields");
-    var productDesignEngineerFields = document.getElementById("productDesignEngineerFields");
     var graphicDesignEngineerFields = document.getElementById("graphicDesignEngineerFields");
     var marketingAdvisorFields = document.getElementById("marketingAdvisorFields");
+    var mobileAppFields = document.getElementById("mobileAppFields"); // Add this line
 
     // Hide all additional fields initially
     dataScientistFields.style.display = "none";
-    productDesignEngineerFields.style.display = "none";
     graphicDesignEngineerFields.style.display = "none";
     marketingAdvisorFields.style.display = "none";
+    mobileAppFields.style.display = "none"; 
 
     // Show additional fields based on the selected option
     if (selectedOption === "dataScientist") {
         dataScientistFields.style.display = "block";
-    } else if (selectedOption === "productDesignEngineer") {
-        productDesignEngineerFields.style.display = "block";
-    } else if (selectedOption === "graphicDesignEngineer") {
+    } else if (selectedOption === "graphicDesigner") {
         graphicDesignEngineerFields.style.display = "block";
-    } else if (selectedOption === "marketingAdvisor") {
+    } else if (selectedOption === "SocialMedia") {
         marketingAdvisorFields.style.display = "block";
+    } else if (selectedOption === "mobileAppDev") { // Add this block
+        mobileAppFields.style.display = "block";
     }
+    
     // Add more conditions for other options as needed
 
     // Rest of your code...
